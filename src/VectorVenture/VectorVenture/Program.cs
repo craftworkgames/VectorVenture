cec8a17cd14deb6188b8dcc0d01a09fae6089d33
Lucas Girouard-Stranks <lustranks@gmail.com>
2020-07-04 19:34:25 -0400

    Clean up code

---
+++ src/VectorVenture/VectorVenture/Program.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Craftwork Games. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+
+namespace VectorVenture
+{
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            using var game = new VectorVentureGame();
+            game.Run();
+        }
+    }
+}
